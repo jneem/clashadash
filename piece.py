f9f8ee1acc26b80117cd5491e164b22954269807
Joe Neeman <joeneeman@gmail.com>
2012-04-13 19:07:33 -0700

    Add more Piece method stubs.

--- piece.py
+++ piece.py
@@ -12,10 +12,39 @@ class Piece:
         
         Params:
           size is a (height, width) tuple.
-          position is a (row, column) tuple.
+          position is a (row, column) tuple, and it refers to the smallest row
+              and column spanned by the piece. (That is, the piece occupies
+              [row, row + width - 1] x [column, column + height - 1].)
         """
         
         self.position = position
         self.size = size
         self.moveable = moveable
+
+    def charging_region(self):
+        """
+        Returns a (height, width) pair. If the rectangle of the given
+        dimensions behind this piece is filled with units of the appropriate
+        color, this piece will become charged.
         
+        A return value of (0, 0) means that this piece is unchargeable.
+        """
+        return (0, 0)
+        
+    def can_charge(self, other):
+        """
+        Returns true if other can be used to charge this.
+        """
+        return False
+        
+    def can_merge(self, other):
+        """
+        Returns true if other can be merged with this.
+        """
+        return False
+        
+    def merge(self, other):
+        """
+        Merges another piece into this one.
+        """
+        pass
